13de6de45ce89916b125f3ccf712a3c45c3bae4a
h8m0ss <danarmenta32@gmail.com>
2022-03-07 22:27:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# website updates
+A python script that sends you an email if a website changes.
+
+Whenever this script is run, it will compare the specified website to a cached version of said website and it will send an email if it can find any changes.
+
+Currently the file will send at most one mail a day, but I am planning to allow that to be customizable as well.
+
+## Installation
+
+Installation is relatively simple, but it is necesary to make a new mail account for the script to use (you could use your own account, but I can't recommend it)
+
+Once you have created a new mail account, it is also important to allow python to access it, this is done differently depending on your provider, so you will have to do some research.
+For gmail, you need to set "allow less secure apps" to ON in order to use this service.
+
+![image](https://user-images.githubusercontent.com/43828996/157164093-fe4b6ccd-d8ca-44b3-81ce-1b084f822efc.png)
+
+Once this is done, you should clone this repository into a machine, and edit the `conf.txt` file
+
+On the first line of the conf file, write the URL you want to check for changes
+
+Then, on the next line, the mail account you created
+
+Then the password of said mail account, this is why we reccomend you use an account separate from your own, although as long as you don't show this file to anyone you should be safe
+
+Then every line onwards should contain a destination email, to forward the data towards.
+
+Here is an example:
+
+```
+https://youtube.com/channel/ludwig/videos
+myRobotAccount@gmail.com
+password12345
+myPersonalMail@gmail.com
+myFriendsPersonalMail@gmail.com
+SomeOtherMail@gmail.com
+```
+
+## Setting up a cron job
+
+It is important to know that this script will only check if you run it (it is also set up to only check once a day at most less time is WIP!), which means you would have to run it mannually each time .
+In order to avoid this, you should set up this script to run automatically, this section explains how to do so
+
+### Windows
+
+In windows, we can set up a scheduled task using the schtasks command, which takes a few important parameters:
+```ps
+schtasks /create /sc HOURLY /tn <SOME NAME> /tr "C:\full\path\to\project\python.exe C:\full\path\to\project\main.py" 
+```
+This will make the check run every hour.
+
+### Linux and Mac OS
+
+Setting up a cron job on Linux and mac is similarly quite easy, we just need to open the cron tab by running the followinf command:
+```
+crontab -e
+```
+This will open the crontab, which is where your cron jobs are stored, to add a new one, go to the last line and add the following (for an hourly check)
+```
+0 * * * * python /path/to/project/dir/main.py
+```
---
+++ main.py
@@ -0,0 +1,129 @@
+import datetime
+import requests
+import smtplib
+import ssl
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+CONF_PATH = CURRENT_DIR + '\\conf.txt'
+LAST_DAY_PATH = CURRENT_DIR + '\\last_day.txt'
+LAST_RESPONSE_PATH = CURRENT_DIR + '\\last_response.txt'
+LOG_PATH = CURRENT_DIR + '\\.log'
+
+with open(CONF_PATH, 'r') as f:
+    global WEBSITE
+    global USERNAME
+    global PASSWORD
+    global TARGET_MAILS
+
+    text = f.read()
+    text = text.split('\n')
+    WEBSITE = text[0]
+    USERNAME = text[1]
+    PASSWORD = text[2]
+    TARGET_MAILS = text[3:]
+
+
+def generate_log(old_data, new_data, equal):
+    target = ''
+    if (len(TARGET_MAILS) > 3):
+        for _target in TARGET_MAILS[:3]:
+            target += str(_target) + ' ,'
+        target = target[:-1]
+        target += f'and {len(TARGET_MAILS) - 3} more'
+    else:
+        for _target in TARGET_MAILS:
+            target += str(_target) + ' ,'
+        target = target[:-1]
+
+    date = str(datetime.datetime.today())
+    textA = f'''\
+
+{date}
+SENT UPDATE TO {target} FROM {USERNAME}
+
+OLD DATA:
+{old_data}
+
+NEW DATA:
+{new_data}
+
+--------------------------------------------------------------------------------
+'''
+    textB = f'''\
+
+{date}
+DATA DID NOT CHANGE; DID NOT SEND MAIL
+
+--------------------------------------------------------------------------------
+'''
+
+    with open(LOG_PATH, 'a') as f:
+        if (not equal):
+            f.write(textA)
+        else:
+            f.write(textB)
+
+
+def get_data():
+    r = requests.get(WEBSITE)
+    return r.text
+
+
+def get_old_data():
+    data = ''
+    with open(LAST_RESPONSE_PATH, 'r') as f:
+        data = f.read()
+    return data
+
+
+def update_data(data):
+    current_day = datetime.datetime.today().day
+    with open(LAST_DAY_PATH, 'w+') as f:
+        f.write(str(current_day))
+
+    with open(LAST_RESPONSE_PATH, 'w+') as f:
+        f.write(str(data))
+
+
+def compare_day():
+    current_day = datetime.datetime.today().day
+    last_day = 0
+    with open(LAST_DAY_PATH, 'r') as f:
+        last_day = int(f.read())
+
+    return last_day != current_day
+
+
+def send_mail():
+    port = 465
+
+    message = f'''\
+Subject: Website updated
+
+I am your website update checker, here to tell you that {WEBSITE} has been updated
+
+This message was sent by the website checker, TODO: put the github link here
+If you don't know why you received this mail, please ignore the mail and add this account to your spam folder
+'''
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+        server.login(USERNAME, PASSWORD)
+        server.sendmail(USERNAME, TARGET_MAILS, message)
+
+
+def main():
+    if compare_day():
+        data = get_data()
+        old_data = get_old_data()
+        is_data_equal = str(data) == str(old_data)
+        generate_log(old_data, data, is_data_equal)
+        if not is_data_equal:
+            update_data(data)
+            send_mail()
+
+
+if __name__ == '__main__':
+    main()
